07935fbec44afd67971f84b2dc42b15a18a5d2c8
LuJoker <jaker743835@gmail.com>
2017-09-25 15:25:07 +0800

    web pack demo
    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import _ from 'lodash'
+import './style.css'
+function component() {
+  var element = document.createElement('div');
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(['<h1>Hello</h1> <p>yoyoyo123</p>', 'webpack'], ' ');
+  element.classList.add('hello')
+  return element;
+}
+
+document.body.appendChild(component());
